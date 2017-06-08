e100a90013351d1e1310b848e556e3b31aa09ef7
baiadori <baiadori@192.168.0.7>
2017-06-08 16:21:24 -0300

    Projeto inicial do sistema!

--- App/Loader.php
+++ App/Loader.php
@@ -1,17 +1,16 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 namespace App;
 
-/**
- * Description of Loader
- * @author baiadori
- */
-
 class Loader {
-    //put your code here
+
+    public function Register() {
+        spl_autoload_register(array($this, 'Autoload'));
+    }
+
+    public function Autoload($Class) {
+        $Class = DIR . DS . str_replace("\\", DS, $Class) . '.php';
+        include_once $Class;
+    }
+
 }
--- Index.php
+++ Index.php
@@ -1,9 +1,40 @@
 <?php
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+header('Content-Type: text/html; charset=utf-8');
 
-echo "Hello word";
+define("DIR", dirname(__FILE__));
+define("DS", DIRECTORY_SEPARATOR);
+define("LOCAL", 'http://obnloja/');
+define("ESTILO", 'Assets/Css/');
+define("JQUERY", 'Assets/Jquery/3.2.1/');
+define("IMG", 'Assets/Imagens/');
 
+$Include = DIR . DS . 'App' . DS . 'Loader.php';
+include_once $Include;
+
+$Loader = new App\Loader();
+$Loader->Register();
+
+//$Controller = new App\Mvc\Controller();
+//$Controller->Index();
+
+$Pdo = new PDO("mysql:host=localhost;dbname=shop", "root", "");
+//$ProductRepository = new App\Model\Product\ProductRepositoryPDO($Pdo);
+
+$Page = isset($_GET['page']) ? $_GET['page'] : '';
+$Action = isset($_GET['action']) ? $_GET['action'] : 'Index';
+
+switch ($Page) {
+    case 'cart' :
+        $Cart = new App\Controller\Cart();
+        call_user_func_array(array($Cart, $Action), array());
+        break;
+
+    case 'login' :
+        $Login = new App\Controller\Login();
+        call_user_func_array(array($Login, $Action), array());
+        break;
+
+    default :
+        $Home = new App\Controller\Home($ProductRepository);
+        call_user_func_array(array($Home, $Action), array());
+}
