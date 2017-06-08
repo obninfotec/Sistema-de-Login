12c3a84248c80640cd0623a0502f03b63ac27e21
baiadori <baiadori@192.168.0.7>
2017-06-08 16:03:28 -0300

    Projeto inicial do sistema!

---
+++ App/Loader.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App;
+
+/**
+ * Description of Loader
+ * @author baiadori
+ */
+
+class Loader {
+    //put your code here
+}
